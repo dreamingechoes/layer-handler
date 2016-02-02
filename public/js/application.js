4844ad302f1ed83e1f6ed78582ec40ce861da166
Ivan Gonzalez <ivan@simplelogica.net>
2016-02-02 13:02:32 +0100

    Deleted public folder with old version.

--- public/js/application.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  // Set debug mode (for console logs)
-  LayerHandler.debug = true;
-
-  LayerHandler.data.gtm_id = 'YOUR_GTM_ID_HERE';
-
-  // Executes the testMethod
-  LayerHandler.testMethod();
-});
