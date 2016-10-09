4cf2e62e50d118ba3eec604d79a5f616bfd5da1d
Ivan Gonzalez <ivan@simplelogica.net>
2016-10-09 19:21:54 +0200

    Delete Layer::Handler Railtie

--- lib/layer/handler/rails/railtie.rb
+++
@@ -1,9 +0,0 @@
-require 'layer/handler/rails/view_helpers'
-
-module Layer::Handler::Rails
-  class Railtie < ::Rails::Railtie
-    initializer "layer-handler" do
-      ActionView::Base.send :include, ViewHelpers
-    end
-  end
-end
