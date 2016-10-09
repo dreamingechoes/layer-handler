dbd7bb336fd6959d926e5a1ef62662df358a37cc
Ivan Gonzalez <ivan@simplelogica.net>
2016-10-09 20:16:31 +0200

    Update src/layer-handler.js and demo/index.html

--- src/layer-handler.js
+++ src/layer-handler.js
@@ -46,12 +46,17 @@
         this.pushData(data);
       }
     }.bind(this));
-  }
+  };
 
   LayerHandler.appendGTM = function() {
-    var gtm_no_script = document.createElement('noscript');
-    gtm_no_script.innerHTML = "<iframe src='//www.googletagmanager.com/ns.html?id='" + this.gtm_id + "'" +
-                              "height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>";
+    var gtm_no_script = document.createElement('noscript'),
+        gtm_no_script_iframe = document.createElement('iframe');
+
+    gtm_no_script_iframe.setAttribute('src', '//www.googletagmanager.com/ns.html?id=' + this.gtm_id);
+    gtm_no_script_iframe.setAttribute('height', '0');
+    gtm_no_script_iframe.setAttribute('width', '0');
+    gtm_no_script_iframe.setAttribute('style', 'display:none;visibility:hidden');
+    gtm_no_script.appendChild(gtm_no_script_iframe);
 
     var gtm_script = document.createElement('script');
     gtm_script.innerHTML = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':" +
@@ -59,18 +64,19 @@
                            "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=" +
                            "'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);" +
                            "})(window,document,'script','dataLayer','" + this.gtm_id + "');";
-                           
-    var appendElement = document.body.lastChild;
-    appendElement.parentNode.insertBefore(gtm_no_script, appendElement);
-    appendElement.parentNode.insertBefore(gtm_script, appendElement);
+
+    var appendHead = document.head.firstChild,
+        appendBody = document.body.firstChild;
+    appendHead.parentNode.insertBefore(gtm_script, appendHead);
+    appendBody.parentNode.insertBefore(gtm_no_script, appendBody);
   };
 
   LayerHandler.pushData = function(data) {
     if(data !== null) {
-      var data = JSON.parse(data),
+      var parsed_data = JSON.parse(data),
           temp = {};
-      for(var key in data) {
-        temp[key] = data[key];
+      for(var key in parsed_data) {
+        temp[key] = parsed_data[key];
       }
       dataLayer.push(temp);
     }
