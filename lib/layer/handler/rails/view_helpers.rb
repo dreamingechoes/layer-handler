b6f555697b902106127258ae919eee77cc8b3df3
Ivan Gonzalez <ivan@simplelogica.net>
2016-10-09 19:21:32 +0200

    Add init_layer_handler rails view_helper

--- lib/layer/handler/rails/view_helpers.rb
+++ lib/layer/handler/rails/view_helpers.rb
@@ -1,11 +1,15 @@
 require 'active_support/core_ext/string/output_safety'
 
-module Layer::Handler::Rails
-  module ViewHelpers
+module Layer
+  module Handler
+    module ViewHelpers
 
-    def gtm_page_attributes data
-      "'data-gtm-page': #{data}".html_safe
+      def init_layer_handler
+        content_tag :script do
+          "LayerHandler.init('#{Layer::Handler.configuration.gtm_id}');".html_safe
+        end.html_safe
+      end
+      
     end
-
   end
 end
